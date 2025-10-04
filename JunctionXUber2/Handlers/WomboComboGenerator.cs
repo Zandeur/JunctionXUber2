9a60ec60e1e2b5d65eca8ba6d2c4e921957ef993
Giesmeister <gijs@kwsmit.nl>
2025-10-04 22:11:22 +0200

    ik ga dit pushen

--- JunctionXUber2/Handlers/WomboComboGenerator.cs
+++ JunctionXUber2/Handlers/WomboComboGenerator.cs
@@ -105,15 +105,15 @@ namespace JunctionXUber2.Handlers
 
             switch (city)
             {
-                case "city1":
+                case "1":
                     return ConditionValue.ConditionType.city1;
-                case "city2":
+                case "2":
                     return ConditionValue.ConditionType.city2;
-                case "city3":
+                case "3":
                     return ConditionValue.ConditionType.city3;
-                case "city4":
+                case "4":
                     return ConditionValue.ConditionType.city4;
-                case "city5":
+                case "5":
                     return ConditionValue.ConditionType.city5;
                 default:
                     return ConditionValue.ConditionType.city1;
@@ -121,7 +121,7 @@ namespace JunctionXUber2.Handlers
         }
 
         private ConditionValue.ConditionType MapToDistance(RowData trip)
-        {
+        { 
             double dist = double.Parse(trip.data["distance_km"]);
             if (dist < 3) return ConditionValue.ConditionType.distance3;
             else if (dist < 7) return ConditionValue.ConditionType.distance37;
